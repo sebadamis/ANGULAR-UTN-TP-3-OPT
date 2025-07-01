cf462acd8f694119797643e739dfbf3e7ae70555
Sebastián Damis <sebastian.damis@gmail.com>
2025-07-01 01:12:35 -0300

    3th

--- src/app/services/products.services.service.ts
+++ src/app/services/products.services.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Products{
+  id: number,
+  titulo: string,
+  descuento: number,
+  precioFinal: number,
+  precioAntes: number,
+  stock: number,
+  img: string,
+  categoria: string,
+  descripcion: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsServicesService {
-
-  constructor() { }
+  // https://fakestoreapi.com/products
+  private API_URL = "../../../public/product.json"
+  constructor(private http: HttpClient) { }
+  getProducts () : Observable<Products[]>{
+    return this.http.get<Products[]>(this.API_URL)
+  }
 }
